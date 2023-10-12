b37342efc9cf8ed1334f5263a1090a3c1ab4ccde
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-10-12 22:09:54 +0700

    feat: change name container and add api

--- web-server/main.go
+++ web-server/main.go
@@ -2,42 +2,12 @@ package main
 
 import (
 	"log"
-	"net/http"
 	"os"
-	"time"
+
+	"os-container-project/internal/handler"
 
 	"github.com/gofiber/fiber/v2"
 )
-
-type APIHandler interface {
-	Handle(c *fiber.Ctx) error
-}
-
-type HealthHandler struct{}
-
-func (h *HealthHandler) Handle(c *fiber.Ctx) error {
-	if err := c.Status(http.StatusOK).JSON(fiber.Map{
-		"message": "API is healthy",
-	}); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-type ArgoHandler struct{}
-
-func (h *ArgoHandler) Handle(c *fiber.Ctx) error {
-	if err := c.Status(http.StatusOK).JSON(fiber.Map{
-		"message":   "CI/CD pipeline is working!",
-		"success":   true,
-		"timestamp": time.Now().Unix(),
-	}); err != nil {
-		return err
-	}
-
-	return nil
-}
 
 func main() {
 	port := os.Getenv("PORT")
@@ -50,13 +20,16 @@ func main() {
 		ProxyHeader:             fiber.HeaderXForwardedFor,
 	})
 
-	healthHandler := &HealthHandler{}
-	argoHandler := &ArgoHandler{}
+	healthHandler := &handler.HealthHandler{}
+	argoHandler := &handler.ArgoHandler{}
+	memberHandler := &handler.MemberHandler{}
+	memberIdHandler := &handler.MemberIdHandler{}
 
 	router.Get("/health", healthHandler.Handle)
 	router.Get("/argo", argoHandler.Handle)
+	router.Get("/member", memberHandler.Handle)
+	router.Get("/member/:id", memberIdHandler.Handle)
 
-	log.Printf("Server is running on port %s", port)
 	if err := router.Listen(":" + port); err != nil {
 		log.Fatalf("Failed to start server: %s", err.Error())
 	}
